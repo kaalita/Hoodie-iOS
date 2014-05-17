1428895e4e8129d012967dac970a4927a1620ddc
Piet Brauer <piet@nerdishbynature.com>
2014-05-17 11:51:59 +0200

    Add expecta

---
+++ Example/Pods/Pods-HoodieExampleTests-Expecta-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_HoodieExampleTests_Expecta : NSObject
+@end
+@implementation PodsDummy_Pods_HoodieExampleTests_Expecta
+@end
---
+++ Example/Pods/Pods-HoodieExampleTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_HoodieExampleTests : NSObject
+@end
+@implementation PodsDummy_Pods_HoodieExampleTests
+@end
